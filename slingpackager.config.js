d59b5ec7abbbe0f80f503f769c625fbdf3e83af5
reusr1 <rr@headwire.com>
2020-04-08 05:24:17 -0700

    build and install process support for node

---
+++ slingpackager.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "vault-properties": {
+        "comment": "myapp - UI Apps",
+        "entry": {
+            "name": "business_sample-full-packag",
+            "version": "1.0",
+            "group": "business_sample"
+        }
+    }
+}
